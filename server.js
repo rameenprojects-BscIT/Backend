16efd5c88fcf9407b4bfc65f73b3880e0f2684b5
Rameen Usman <rameenProjects@gmail.com>
2025-11-24 01:44:23 +0400

    settled the basic  Express server, middleware, and CORS headers.

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+// creating an express application instance to define routes
+const app = express();
+app.set('PORT', 3000);
+
+// added middleware to the app that automatically parses incoming JSON requests
+app.use(express.json());
+
+// setting the cors headers
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+ 
+    next();
+});
+
+app.listen(3000, () => {
+    console.log("express.js server is running on localhost:3000");
+});
